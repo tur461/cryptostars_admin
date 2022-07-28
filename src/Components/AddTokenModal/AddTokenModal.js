356dced2ba7c82627a2a29e829f86b1201deed7e
ashwani83602 <ashwani.ashwani@antiersolutions.com>
2022-07-28 12:47:31 +0530

    validation with alignment

--- src/Components/AddTokenModal/AddTokenModal.js
+++ src/Components/AddTokenModal/AddTokenModal.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./AddTokenModal.scss";
 import "../../App.scss";
@@ -6,6 +6,7 @@ import "../../Pages/Public/Swap/Swap.scss";
 import back from "../../assets/images/back-arrow.svg";
 import { Modal, FormLabel, Button } from "react-bootstrap";
 import PreviewAddTokenModal from "./PreviewAddTokenModal";
+import Web3 from "web3";
 
 function AddTokenModal() {
   let refrenceVariable = false;
@@ -22,34 +23,83 @@ function AddTokenModal() {
   const [supplyError, setSupplyError] = useState("");
   const [mintAddressError, setMintAddressError] = useState("");
   const [ownerAddressError, setOwnerAddressError] = useState("");
-
-
-
-
-
+  const [error, setError]= useState(false);
+  const [symbolsArr] = useState(["e", "E", "+", "-"]);
 
   const handleChange = (e, fieldName) => {
     switch ((e, fieldName)) {
       case "tokenName":
-        e.target.value==''?setnameError("Please Enter Token Name"):setnameError('')
-        setTokenName(e.target.value);
+        const { value } = e.target;
+        console.log("Input value: ", value);
+        const re = /^[A-Za-z]+$/;
+
+        if (value === "" || re.test(value)) {
+          setTokenName(value);
+        }
+        value == ""
+          ? setnameError("Please Enter Token Name")
+          : setnameError("") ; setError(false);
+
+        // setTokenName(e.target.value);
+
         break;
       case "tokenSymbol":
-        e.target.value==''?setSymbolError("Please Enter Token Name"):setSymbolError('')
-        setTokenSymbol(e.target.value);
+        console.log("mmmmmmmmmmmmmrrrrrrrrrr", e.target.value);
+        const value1 = e.target.value;
+        console.log("Input value1111111111: ", value1);
+        const re1 = /^[A-Za-z]+$/;
+        if (value1 === "" || re1.test(value1)) {
+          setTokenSymbol(value1);
+        }
+        value1 == ""
+          ? setSymbolError("Please Enter Token Name")
+          : setSymbolError("")  ; setError(false);
         break;
       case "totalSupply":
-        e.target.value==''?setSupplyError("Please Enter Token Supply"):setSupplyError('')
+        e.target.value == ""
+          ? setSupplyError("Please Enter Token Supply")
+          : setSupplyError("")  ; setError(false);
         setTotalSupply(e.target.value.toString());
+
         break;
       case "mintAddress":
-        e.target.value==''?setMintAddressError("Please Enter Mint Address"):setMintAddressError('')
-        setMintAddress(e.target.value);
+        e.target.value == ""
+          ? setMintAddressError("Please Enter Mint Address")
+          : setMintAddressError("")  ; setError(false);
+        try {
+          const isValidAddress = Web3.utils.toChecksumAddress(e.target.value);
+          if (isValidAddress || e.target.value) {
+            setMintAddress(e.target.value);
+            setMintAddressError("")  ; setError(false);
+          }
+        } catch (err) {
+          setError(true);
+          console.error("invalid ethereum address", err.message);
+          setMintAddress(e.target.value);
+          setMintAddressError("Please  Enter correct Mint Address");
+        }
         refrenceVariable = true;
+
         break;
       case "ownerAddress":
-        e.target.value==''?setOwnerAddressError("Please Enter Owner Address"):setOwnerAddressError('')
-        setOwnerAddress(e.target.value);
+        e.target.value == ""
+          ? setOwnerAddressError("Please Enter Mint Address")
+          : setOwnerAddressError("")  ; setError(false);
+        try {
+          const isValidAddress = Web3.utils.toChecksumAddress(e.target.value);
+          if (isValidAddress || e.target.value) {
+            setOwnerAddress(e.target.value);
+            setOwnerAddressError("")  ; setError(false);
+          }
+        } catch (err) {
+          setError(true);
+          console.error("invalid ethereum address", err.message);
+          setOwnerAddress(e.target.value);
+          setOwnerAddressError("Please  Enter correct Mint Address");
+        }
+
+        // e.target.value==''?setOwnerAddressError("Please Enter Owner Address"):setOwnerAddressError('')
+        // setOwnerAddress(e.target.value);
         refrenceVariable = true;
         break;
       default:
@@ -60,43 +110,33 @@ function AddTokenModal() {
   const handleShow = () => setShow(true);
   console.log("token ka naam", tokenName);
 
-
-   const Preview = () =>{
-    if(tokenName=='')
-    {
-     setnameError("Please Enter Token Name")
+  const Preview = () => {
+    if (tokenName == "") {
+      setnameError("Please Enter Token Name");
+      setError(true);
     }
-    else if(tokenSymbol=='')
-    {
-      setSymbolError("Please Enter Token Symbol")
+    if (tokenSymbol == "") {
+      setSymbolError("Please Enter Token Symbol");
+      setError(true);
     }
-    else if(totalSupply=='')
-    {
-      setSupplyError("Please Enter Token TotalSupply") 
+    if (totalSupply == "") {
+      setSupplyError("Please Enter Token TotalSupply");
+      setError(true);
     }
-    else if(mintAddress=='')
-    {
-      setMintAddressError("Please Enter Mint Address")
+    if (mintAddress == "") {
+      setMintAddressError("Please Enter Mint Address");
+      setError(true);
     }
-    else if(ownerAddress=='')
-    {
-      setOwnerAddressError("Please Enter Owner Address")
+    if (ownerAddress == "") {
+      setOwnerAddressError("Please Enter Owner Address");
+      setError(true);
     }
-
-    else{
-      setShow(true)
+console.log("rrrrrrrrrrrrr",error);
+    if (error == false && tokenName!=='' & tokenSymbol!='' & totalSupply!='' & mintAddress!='' & ownerAddress!='') {
+      
+       setShow(true);
     }
-    
-    
-   }
-    // if(tokenName=='')
-    // {
-    //   setnameError("Please Enter Token Name")
-    // }
-  
-  
-
-
+  };
 
   return (
     <>
@@ -112,58 +152,74 @@ function AddTokenModal() {
                 <li>
                   <div className="token_info d-flex mb-3">
                     <FormLabel className="text_head">Token Name</FormLabel>
-                    <input
-                      label="Token Name"
-                      type="text"
-                      value={tokenName}
-                      onChange={(e) => handleChange(e, "tokenName")}
-                    />
-              
+                    <div className="input_text">
+                      <input
+                        label="Token Name"
+                        type="text"
+                        value={tokenName}
+                        onKeyPress={(event) =>
+                          (event.code >= 65 && event.code <= 90) ||
+                          (event.code >= 97 && event.code <= 122)
+                        }
+                        onChange={(e) => handleChange(e, "tokenName")}
+                      />
+                      <span>{nameError}</span>
+                    </div>
                   </div>
-
-                  <span style={{color:'red',}}>{nameError}</span>
                 </li>
 
                 <li>
                   <div className="token_info d-flex mb-3">
                     <FormLabel className="text_head">Token Symbol</FormLabel>
-                    <input
-                      label="Token Name"
-                      type="text"
-                      value={tokenSymbol}
-                      onChange={(e) => handleChange(e, "tokenSymbol")}
-                    />
+                    <div className="input_text">
+                      <input
+                        label="Token Name"
+                        type="text"
+                        value={tokenSymbol}
+                        onChange={(e) => handleChange(e, "tokenSymbol")}
+                      />
+                      <span>{symbolError}</span>
+                    </div>
                   </div>
-                  <span style={{color:'red'}}>{symbolError}</span>
                 </li>
 
                 <li>
                   <div className="token_info d-flex mb-3">
                     <FormLabel className="text_head">Token Supply</FormLabel>
+                    <div className="input_text">
                     <input
                       label="Token Name"
                       type="Number"
                       value={totalSupply}
+                      onKeyDown={(evt) => {
+                        symbolsArr.includes(evt.key) && evt.preventDefault();
+                      }}
                       onChange={(e) => handleChange(e, "totalSupply")}
                     />
+                    <span>{supplyError}</span>
+                    </div>
                   </div>
-                  <span style={{color:'red'}}>{supplyError}</span>
+                  
                 </li>
                 <li>
                   <div className="token_info d-flex mb-3">
                     <FormLabel className="text_head">Mint Address</FormLabel>
+                    <div className="input_text">
                     <input
                       label="Token Name"
                       type="text"
                       value={mintAddress}
                       onChange={(e) => handleChange(e, "mintAddress")}
                     />
+                  <span>{mintAddressError}</span>
                   </div>
-                  <span style={{color:'red'}}>{mintAddressError}</span>
+                  </div>
+                  
                 </li>
                 <li>
                   <div className="token_info d-flex mb-3">
                     <FormLabel className="text_head">Owner Address</FormLabel>
+                    <div className="input_text">
                     <input
                       label="Token Name"
                       type="text"
@@ -171,14 +227,14 @@ function AddTokenModal() {
                       onChange={(e) => handleChange(e, "ownerAddress")}
                       required="true"
                     />
-                     
+                  <span>{ownerAddressError}</span>
                   </div>
-                  <span style={{color:'red'}}>{ownerAddressError}</span>
+                  </div>
                 </li>
               </ul>
 
               <div className="token_footer">
-                <Button variant="secondary" onClick={Preview}>
+                <Button variant="secondary"  onClick={Preview}>
                   Preview
                 </Button>
                 a
