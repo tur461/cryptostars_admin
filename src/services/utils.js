e6eebf800d98ddf48762c4ad4d7dfdb54a9c00b4
tur461 <tur461.er@gmail.com>
2022-08-23 16:17:01 +0530

    cherry pick commit

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,63 +1,3 @@
-// const toB64 = s => btoa(s);
-
-// const eHandle = e => e.preventDefault() || !0;
-
-
-// const range = n => '0'.repeat(n).split('').map((_, i) => i + 1);
-
-// const truncAddr = addr => `${addr.slice(0, 5)}...${addr.slice(38, 42)}`;
-
-// const sortArr = arr => {
-//     if(arr[0]) {
-//         if(typeof arr[0] === 'string') {
-//             return arr.sort((a, b) => {
-//                 a = a.toLowerCase();
-//                 b = b.toLowerCase();
-//                 return a < b ? 1 : a > b ? -1 : 0;
-//             })
-//         } else return arr;
-//     }
-// }
-
-// function selectText(node) {
-//     if (document.body.createTextRange) {
-//         const range = document.body.createTextRange();
-//         range.moveToElementText(node);
-//         range.select();
-//     } else if (window.getSelection) {
-//         const selection = window.getSelection();
-//         const range = document.createRange();
-//         range.selectNodeContents(node);
-//         selection.removeAllRanges();
-//         selection.addRange(range);
-//     } else {
-//         console.warn("Could not select text in node: Unsupported browser.");
-//     }
-// }
-
-
-// const LocalStore = {
-//     has: key => key in localStorage,
-//     get: key => localStorage.getItem(key),
-//     del: key => localStorage.removeItem(key),
-//     add: (key, value) => localStorage.setItem(key, value), 
-
-// }
-
-// const toHex = n => `${n}`.indexOf('0x') > -1 ? n : `0x${Number(n).toString(16)}`;
-
-// export {
-//     toHex,
-//     toB64,
-//     range,
-//     sortArr,
-//     eHandle,
-//     truncAddr,
-//     LocalStore,
-//     selectText,
-// }
-
-
 import { ADDRESS } from "../constant";
 
 const toB64 = s => btoa(s);
@@ -83,6 +23,20 @@ function empty(v) {
 const notEmpty = v => !empty(v);
 const contains = (s, q) => notEqual(s.toLowerCase().indexOf(q), -1);
 
+const range = n => '0'.repeat(n).split('').map((_, i) => i + 1);
+
+const sortArr = arr => {
+    if(arr[0]) {
+        if(typeof arr[0] === 'string') {
+            return arr.sort((a, b) => {
+                a = a.toLowerCase();
+                b = b.toLowerCase();
+                return a < b ? 1 : a > b ? -1 : 0;
+            })
+        } else return arr;
+    }
+}
+
 function selectText(node) {
     if (document.body.createTextRange) {
         const range = document.body.createTextRange();
@@ -105,6 +59,13 @@ function isAddr(addr) {
     addr.length === 42 && 
     notEqual(addr, ADDRESS.ZERO);
 }
+const LocalStore = {
+    has: key => key in localStorage,
+    get: key => localStorage.getItem(key),
+    del: key => localStorage.removeItem(key),
+    add: (key, value) => localStorage.setItem(key, value), 
+
+}
 
 const toHex = n => `${n}`.indexOf('0x') > -1 ? n : `0x${Number(n).toString(16)}`;
 
@@ -122,5 +83,8 @@ export {
     contains,
     eHandle,
     notEmpty,
+    range,
+    sortArr,
+    LocalStore,
     selectText,
 }
